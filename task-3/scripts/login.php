bea6bcd80ba0ac2f63e05a4edb4340d13428e0a7
palak5134 <82887211+palak5134@users.noreply.github.com>
2021-08-28 00:42:33 +0530

    Add files via upload

---
+++ task-3/scripts/login.php
@@ -0,0 +1,31 @@
+<?php
+$expire=time()+60*60*24*30;
+include '../classes/dbh.class.php';
+
+if (isset($_POST['adminLogin'])){
+    $username=$_POST['username'];
+    $password=$_POST['password']; 
+    $admin=new Admin();
+    $admin_details=$admin->getAllAdmin();
+    foreach ($admin_details as $credentials){
+
+        if($credentials['password']==$password && $credentials['username']==$username){
+            header("location:../admin/main-dashboard.php");
+            //setting cookies
+            setcookie('user',$credentials['username'],$expire,'/');
+            $cookiedata=hash('ripemd160',$credentials['fullname']);
+            setcookie('admin',$cookiedata,$expire,'/');
+            break;   
+        }
+        else{
+            header("Location: ../admin-login.php?message=Wrong credentials");
+
+        } 
+    }
+}
+if (isset($_POST['logout'])){
+    setcookie('user',"",time()-3600,'/');
+    setcookie('admin',"",time()-3600,'/');
+    header("Location: ../index.php");
+
+}
